4c32706da60f29c97aa20d7b7b09ddf8147e53bd
jo <chjo0330@gmail.com>
2022-03-05 18:01:34 +0900

    파일 디렉토리 구조 변경

---
+++ code_recycle/lib/widgets/2. 데이터테이블/data_table_sample.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataTableSample extends StatelessWidget {
+  const DataTableSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hello World!")),
+    );
+  }
+}
